7466ef7e8c5d4a5eb6a5545bc26ed0d93669d663
najmeh98 <foroughi.najmeh98@gmail.com>
2022-12-12 21:01:49 +0330

    change content of tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // content: ["./src/**/*.{html,js}"],
-  content: ["./*.html"],
+  // content: ["./*.html"],
+  content: ["./**/*.{html,js}"],
 
   theme: {
     extend: {
